0e4df9f21fdd9d93999326f1a42ce61eb95afe4a
GeumBongGeum <51769583+bonggeum@users.noreply.github.com>
2020-03-27 16:04:36 +0900

    test

---
+++ test/script.js
@@ -0,0 +1,11 @@
+const wrapper = document.querySelector('.wrapper')
+
+const onMouseMove = ({clientX}) => {
+  const x = (clientX / innerWidth * 2 - 1) * (innerWidth / 4)
+
+  TweenLite.to('.first__name .text__name, .second__name .text__name', 1, {
+    x: x,
+    ease: Sine.easeOut
+  })
+}
+wrapper.addEventListener('mousemove', onMouseMove)
